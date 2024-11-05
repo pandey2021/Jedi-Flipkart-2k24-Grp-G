61ecad9cc8672506f9f465313db730ff91808ef6
pandey2021 <84921465+pandey2021@users.noreply.github.com>
2024-11-05 15:39:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo{
     public static main void(String args[]){
-        System.out.println("This is my first project.")
+        System.out.println("This is my second project.");
     }
 }
